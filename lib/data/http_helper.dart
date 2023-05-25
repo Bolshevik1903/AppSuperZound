271d79c78f61280c8b2e597031db7ba45a32c34a
uwu <uwu@upc.edu.pe>
2023-05-25 14:27:57 -0500

    Added Tracks

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:music_flutter/data/album.dart';
+import 'package:music_flutter/data/track.dart';
 
 class HttpHelper {
+  
   final String urlBase = 'https://theaudiodb.com/api/v1/json/523532/mostloved.php?format=album';
+  final String urlBaseTrack = 'https://theaudiodb.com/api/v1/json/523532/track.php?m=2109889';
 
   Future<List<Album>?> getAlbums() async{
     http.Response response = await http.get(Uri.parse(urlBase));
@@ -15,6 +18,19 @@ class HttpHelper {
       final List<dynamic> albumsMap = jsonResponse['loved'];  
       final List<Album> albums = albumsMap.map((map) => Album.fromJson(map)).toList();
       return albums;
+    } else {
+      return null;
+    }
+  }
+
+   Future<List<Track>?> getTracksByAlbumId(String albumId) async {
+    http.Response response = await http.get(Uri.parse(urlBaseTrack+albumId));
+
+    if (response.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(response.body);
+      final List tracksMap = jsonResponse['track'];
+      final List<Track> tracks = tracksMap.map((e) => Track.fromJson(e)).toList();
+      return tracks;
     } else {
       return null;
     }
